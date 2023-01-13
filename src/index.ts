2d5b86b8c86c7b955e1f42e4152b3983f805acd4
LemoFire <lemofire@gmail.com>
2023-01-13 14:42:41 +0800

    feat: add console docs tip

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,10 @@ export * from "./Identifier";
 export * from "./Optimization";
 export * from "./StringFilter";
 export * from "./Time";
+
+const version = "1.2.2";
+
+console.log(
+  "%cNTANG toolkit " + version + " loaded | https://docs.ito.fun/ntang",
+  "color:#fff; background: linear-gradient(270deg, #986fee, #8695e6, #68b7dd, #18d7d3); padding: 8px 15px; border-radius: 0 15px 0 15px"
+);
